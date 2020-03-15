88e064082175507b33709b8d899e52bcabbe0abd
胡晓峰 <604652763@qq.com>
2020-03-15 20:59:13 +0800

    看文档的笔记和测试

---
+++ document-note/programe/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+module.exports={
+  entry:'./src',
+  output:{
+    filename:'main.js',
+    path:path.resolve(__dirname,'dist')
+  },
+  module:{
+    rules:[{
+      test:/\.css$/,
+      use:['style-loader','css-loader']
+    }]
+  }
+}
